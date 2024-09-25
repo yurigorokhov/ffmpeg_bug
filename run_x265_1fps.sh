4ad091b1f906e2e3a8765c9b8e0970c4a851a1fc
Yuri Gorokhov <yurig@apple.com>
2024-09-25 13:07:35 -0700

    Adding example of current failure

---
+++ run_x265_1fps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eux -o pipefail
+
+VERSION=7.0
+
+# build docker images
+docker build --build-arg FFMPEG_VERSION=ffmpeg-${VERSION} -t ffmpeg-${VERSION} .
+
+
+# generate video with frozen frames
+docker run -v `pwd`:/work --rm -it ffmpeg-${VERSION} \
+    ffmpeg -y -hide_banner -r 1/1 -ignore_editlist true -loglevel error -i /work/input_test_long.mov -an /work/input_test_long_1fps.mov
+
+
+# generate video with frozen frames
+docker run -v `pwd`:/work --rm -it ffmpeg-${VERSION} \
+    ffmpeg -i /work/input_test_long_1fps.mov -c:v libx265 -vf "setpts=(N/(30*TB)),format=pix_fmts=yuv420p" \
+    -tag:v hvc1 \
+    /work/output-test-long-${VERSION}.mov
